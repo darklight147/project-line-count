f2a2e4ef076b0d48b3ded5f9eb48b9d4d08add3b
Mohamed Belkamel <belkamelmohamed@gmail.com>
2020-11-20 23:05:32 +0100

    Codded a recursive function that counts the lines of code in a given directory recursivly, meaning it would go into each folder and subfolder until it finds a file

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import fs from 'fs';
+
+let currentDirectory = (process.argv[2] && `${process.argv[2]}`) || __dirname;
+
+const tobeIgnored = [...process.argv].slice(3, process.argv.length);
+tobeIgnored.length &&
+	console.log(
+		'\n\n\nIgnoring the directories that include the keywords: ' +
+			tobeIgnored.join(', ') +
+			'\n\n\n\n'
+	);
+
+const readDirectory = (path: string) => {
+	return fs.readdirSync(currentDirectory).length
+		? fs.readdirSync(currentDirectory)
+		: [];
+};
+
+let sum = -1;
+
+let linesSum = 0;
+
+try {
+	const countSubFolders = (path: string) => {
+		const init = readDirectory(path);
+		// console.log(init);
+		init.forEach((fileorFolder) => {
+			const currentPath = `${path}/${fileorFolder}`;
+			const isDirec =
+				fs.existsSync(currentPath) && fs.lstatSync(currentPath).isDirectory();
+			if (
+				!tobeIgnored ||
+				!tobeIgnored.some((tb) => fileorFolder.includes(tb))
+			) {
+				if (isDirec) {
+					currentDirectory = currentPath;
+					// console.log(currentPath);
+					countSubFolders(currentPath);
+				} else {
+					const tmpp = fs.readFileSync(currentPath).toString().split('\n')
+						.length;
+					console.log(currentPath, tmpp);
+					linesSum += tmpp;
+				}
+			}
+			sum++;
+		});
+		return sum;
+	};
+
+	countSubFolders(currentDirectory);
+	console.log('\n\nTotal lines found ' + linesSum);
+} catch (error) {
+	console.log(error);
+}
